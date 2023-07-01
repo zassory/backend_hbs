c1b6460fc3464b49d00d67e7f14de6486d5552f9
Nicolas Caceres Latorre <nicolas.programador@gmail.com>
2023-07-01 16:22:52 -0400

    Recursos estaticos

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-const { leerTodo } = require('./services/productosService');
+const express = require('express');
+const { response } = require('express');
+const app = express();
 
-console.log(leerTodo('products'));
+app.use(express.static('public'));
+
+// app.get('/clientes', (req,res = response) => {
+//         res.send('clientes.html');
+// });
+
+// app.get('/empleados' , (req,res = response) => {
+//     res.send('Hola Mundo Empleados');
+// });
+
+app.get('*', (req,res = response) => {
+    res.sendFile(__dirname + '/error.html');
+});
+
+app.get('/detalleDeOrden' , (req,res = response) => {
+    res.send('Hola Mundo Detalle');
+});
+
+app.get('/ordenes' , (req,res = response) => {
+    res.send('Hola Mundo Ordenes');
+});
+
+app.get('/productos' , (req,res = response) => {
+    res.send('Hola Mundo Productos');
+});
+
+app.listen(8080);
